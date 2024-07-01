0c14cf57bf9c300fe5e49b06a5a38ce12c5e951b
Grace_Awguagbaka <awgracy4real@yahoo.com>
2024-07-01 20:24:11 +1200

    Update next.config.mjs for static export and GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/Game' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/Game' : '',
+    trailingSlash: true,
+  };
+  
+  export default nextConfig;
